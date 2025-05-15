d1751a9b986c8e5214e23e1ed88c53f54f158ece
steven-scart <yojit_kumar@proton.me>
2025-05-15 13:56:50 +0530

    fixed concatenation issue, there should be no axis

--- codes/METCvsCoupling.py
+++ codes/METCvsCoupling.py
@@ -15,28 +15,28 @@ def simulate(p, eps, n):
         Y[i] = (1 - eps) * tent_maps(Y[i-1], p) + eps * X[i-1]
     return X, Y
 
-def metc_self(x, y, bins=2):
-    Cx = etc(x, num_bins=bins, normalized=True)
-    Cy = etc(y, num_bins=bins, normalized=True)
-    Cxy = etc(x+y, num_bins=bins, normalized=True)
+def metc_self(x, y, bins=2, normalized=True, verbose=False):
+    Cx = etc(x, num_bins=bins, normalized=normalized, verbose=verbose)
+    Cy = etc(y, num_bins=bins, normalized=normalized, verbose=verbose)
+    Cxy = etc(np.concatenate((x,y),axis=None), num_bins=bins, normalized=normalized, verbose=verbose)
 
     return Cx + Cy - Cxy
 
-def metc(x, y, bins=2):
-    x = ETC.partition(x, n_bins=bins)
-    y = ETC.partition(y, n_bins=bins)
+#def metc(x, y, bins=2):
+#    x = ETC.partition(x, n_bins=bins)
+#    y = ETC.partition(y, n_bins=bins)
 
-    ETCx = ETC.compute_1D(x, verbose=False).get('NETC1D')
-    ETCy = ETC.compute_1D(y,verbose=False).get('NETC1D')
-    ETCxy = ETC.compute_2D(x,y,verbose=False).get('NETC2D')
+#    ETCx = ETC.compute_1D(x, verbose=False).get('NETC1D')
+#    ETCy = ETC.compute_1D(y,verbose=False).get('NETC1D')
+#    ETCxy = ETC.compute_2D(x,y,verbose=False).get('NETC2D')
 
-    return ETCx + ETCy - ETCxy
+#    return ETCx + ETCy - ETCxy
 
 if __name__ == '__main__':
     p = 0.4999
     epsilons = np.linspace(0,1,21)
-    trials = 50
-    n = 15
+    trials = 50 
+    n = 100
 
     metcs = np.zeros_like(epsilons)
 
@@ -45,7 +45,7 @@ if __name__ == '__main__':
       
         for _ in range(trials):
             X, Y = simulate(p, eps, n)
-            metc_vals.append(metc_self(X, Y))
+            metc_vals.append(metc_self(X, Y, normalized=True))
         metcs[idx] = np.mean(metc_vals)
 
     plt.figure(figsize=(8, 8))
